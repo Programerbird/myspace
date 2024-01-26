e2db57b936b2a645731c991709c40ac0d01fd9ab
Water Palma <programerbird@gmail.com>
2024-01-25 19:46:43 -0800

    update3

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,7 @@ function Header() {
             <Button className='b1' variant="contained">Home</Button>
             <Button className='b1' variant="contained">Music</Button>
             <Button className='b1' variant="contained">Video</Button>
+            <Button className='b1' variant="contained">Video</Button>
           </div>
         <div className='search' >  
         <input  type="text" placeholder="Search.."  />
